4672f6abd6db7ef9019a977e11f8cf4c4fc09233
Andrea Centeno <andreacent8@gmail.com>
2017-02-11 16:16:02 -0400

    cambio de color de flecha

--- Assets/scripts/ChangeColorArrow.cs
+++ Assets/scripts/ChangeColorArrow.cs
@@ -5,15 +5,6 @@ public class ChangeColorArrow : MonoBehaviour {
 
 	public Material[] material;
 	Renderer rend;
-	private GameObject theCamera = null;
-
-	void OnEnable () {
-		FindCamera();
-	}
-
-	void FindCamera () {
-		theCamera = GameObject.FindWithTag("MainCamera");
-	}
 	
 	void Start () {
 		rend = transform.parent.Find("arrow").GetComponent<Renderer>();
@@ -25,10 +16,11 @@ public class ChangeColorArrow : MonoBehaviour {
 		ChangeColor();
 	}
 	
+	void OnDisable() {
+		rend.sharedMaterial = material[0];
+	}
+	
 	void ChangeColor() {
-		if(theCamera == null) {
-			FindCamera();
-		}
 		rend.sharedMaterial = material[1];
 	}	
 }
