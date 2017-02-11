d3d4dedd64999661e64b0c4e4ea7f913a38264b3
Enrique <eiglesias34@gmail.com>
2017-02-11 16:02:57 -0430

    Se creo el Branch Player_Enrique

---
+++ Assets/arrow_right.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow_right : MonoBehaviour {
+
+	public Color color = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ChangeColor());
+	}
+	
+	IEnumerator ChangeColor()
+	{
+		color = gameObject.GetComponent<Renderer> ().material.color;
+		while (true) {
+			yield return new WaitForSeconds(3);
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+			Debug.Log("time started");
+			yield return new WaitForSeconds(2);
+			Debug.Log("time ended");
+			gameObject.GetComponent<Renderer> ().material.color = color;
+			yield return new WaitForSeconds(3);
+		}
+	}
+}
---
+++ Assets/arrow_ul.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow_ul : MonoBehaviour {
+
+	public Color color = Color.black;
+	public int count_ul;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ChangeColor());
+		count_ul = 0;
+	}
+		
+
+	// Update is called once per frame
+	//void Update () { 
+		
+	//	ChangeColor ();
+	//}
+
+	IEnumerator ChangeColor()
+	{
+		color = gameObject.GetComponent<Renderer> ().material.color;
+		while (true) {
+			yield return new WaitForSeconds(4);
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+			if(gameObject.GetComponent<Renderer>().material.color == Color.red){
+				count_ul += 1;
+			}
+			Debug.Log("time started");
+			yield return new WaitForSeconds(2);
+			Debug.Log("time ended");
+			gameObject.GetComponent<Renderer> ().material.color = color;
+			yield return new WaitForSeconds(4);
+		}
+	}
+		
+}
