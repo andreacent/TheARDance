d78b11475b14f939c8eadd0a305906fe5b4d5b48
Andrea Centeno <andreacent8@gmail.com>
2017-02-12 12:01:16 -0400

    animacion cuando gana

--- Assets/scripts/DancerController.cs
+++ Assets/scripts/DancerController.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 public class DancerController : MonoBehaviour {
 	public static DancerController Instance { get; private set; }
 
+	private int movements = 0;
 	private GameObject[] markers;
 	public int speed = 3;
 
 	public bool gameHasStarted = false;
 	public bool gameIsDone = false;
 
-	public float time_song = 0.95f;
-	public float time = 0f;
+	private float time_song = 0.95f;
+	private float time = 0f;
 	public Text gameover;
 
 	void Awake(){
@@ -41,8 +42,15 @@ public class DancerController : MonoBehaviour {
 			yield return new WaitForSeconds(speed);
 			markers[act].transform.GetComponent<ArrowBehaviors>().active = false;
 			Debug.Log("DancerController:: marker "+act+" desactive");
+			movements += 1;
 		}
-		gameover.text = "GAME OVER";
+		
+		// El jugador gana si logra mas de 90% de los movimientos
+		if(ScoreManager.score >= movements * 90 * (0.01)){
+			gameover.text = "YOU WIN";
+		}else{
+			gameover.text = "GAME OVER";
+		}
 	}
 	
 	public void GameStart() {
